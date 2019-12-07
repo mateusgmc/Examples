a8ae35ae35a1c27130ef9a9a1ef9ea17f6b00b47
Mateus Mauricio <mateus10.m@live.com>
2019-12-07 20:48:45 -0300

    First

---
+++ src/Examples.DecimalHelper/Examples.DecimalHelper/Helpers/DecimalExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Examples.DecimalHelper.Helpers
+{
+    public static class DecimalExtension
+    {
+        /// <summary>
+        /// Trunca um número decimal baseado na precisão de casas decimais
+        /// </summary>
+        /// <param name="precision">O número de casas decimais que o valor será truncado</param>
+        public static decimal Truncate(this decimal value, int precision)
+        {
+            decimal step = (decimal)Math.Pow(10, precision);
+            decimal aux = Math.Truncate(step * value);
+            return aux / step;
+        }
+    }
+}
---
+++ src/Examples.DecimalHelper/Examples.DecimalHelper/Program.cs
@@ -0,0 +1,18 @@
+﻿using Examples.DecimalHelper.Helpers;
+using System;
+
+namespace Examples.DecimalHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal num1 = 12.129997932m;
+            decimal num2 = 120.349958m;
+
+            Console.WriteLine($"num1 = {num1.Truncate(4)}"); // imprime 12.1299
+            Console.WriteLine($"num2 = {num2.Truncate(3)}"); // imprime 120.349
+            Console.WriteLine($"num3 = {99.122958m.Truncate(5)}"); // imprime 99.12295
+        }
+    }
+}
